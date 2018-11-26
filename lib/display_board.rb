9b43d8c9d7e97965914b14e727de5f8609dc0675
brenda falconer <brendafal19@gmail.com>
2018-11-26 09:49:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,2 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+authenticate_or_request_with_http_basic# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+authenticate_or_request_with_http_basic
